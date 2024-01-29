3faafc55c292ce429bc2d1606e20fa1fe9b98632
moonl1ch <niki.vershkov@gmail.com>
2024-01-29 13:14:59 +0300

    написал программу используя if....else

--- task2/main.js
+++ task2/main.js
@@ -1,1 +1,11 @@
-ллллл
+let temperature = 8;
+
+if (temperature > 30) {
+  console.log("Вы можете надеть легкую одежду.");
+} else if (temperature > 20) {
+  console.log("Нужно надеть теплую одежду.");
+} else if (temperature < 10) {
+  console.log("Сегодня холодно, нужно надеть зимнюю одежду.");
+} else {
+  console.log("Температура находится в норме.");
+}
