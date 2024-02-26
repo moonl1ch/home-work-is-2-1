92da1885ad9f80fdaa9f2132870899e7665fce86
moonl1ch <niki.vershkov@gmail.com>
2024-02-26 10:54:58 +0300

    Выполнил практическую используя функции

---
+++ task6/app.js
@@ -0,0 +1,10 @@
+// 2 Вариант 
+
+const multiply = (num1, num2) => num1 * num2;
+
+const result = multiply(5, 3);
+if (result) {
+  console.log(result); 
+} else {
+  console.log('Результат не определен');
+}
